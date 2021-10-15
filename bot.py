28c0929b2da0491e5ab5833ff7550890fe563124
Dheemn <42458390+Dheemn@users.noreply.github.com>
2021-10-15 19:20:28 +0530

    Update with little bits and bytes
    Added feature to add and remove permission to role members and added help to missionAdd

--- bot.py
+++ bot.py
@@ -26,32 +26,75 @@ class CommonFuncs():
         return False
 
 
-class MissionBot(discord.Client, CommonFuncs):
+intents = discord.Intents(messages = True, members = True, guilds = True)
+bot = commands.Bot(command_prefix='$', intents=intents)
 
-    def __init__(self):
-        self._roleid = ['876015863663308821']
-        super().__init__()
+funcs = CommonFuncs()
 
-    async def on_ready(self):
-        print("Logged in as {0.user}".format(client))
+suRoles = ['']
+allowed_roles = ['']
 
-    async def on_message(self, message):
-        if message.author == client.user:
-            return
+@bot.event
+async def on_ready():
+    print("Logged in as {0.name}{0.id}".format(bot.user))
 
-        if message.content.startswith("$missionAdd"):
-            if CommonFuncs.checkPerms(self, message):
-                await message.channel.send("This is the cmd to create coalition missions!")
-            else:
-                await message.channel.send("You need appropriate permissions")
+@bot.command()
+async def addRole(ctx, *, roleid=None):
+    print(f"$addRole request by {ctx.author}")
+    if roleid is None:
+        await ctx.send("```Help: This command gives permission to role members to post Coalition Missions\nUsage: $addRole <roleID of role>```")
+        return
+    #checks perms with su users i.e discord admins
+    if funcs.checkPerms(ctx, suRoles):
+        allowed_roles.append(str(roleid))
+        await ctx.send(f'RoleID {roleid} has been added')
+    else:
+        await ctx.send("You do not have neccesary permissions")
+    #Having some issues with this code line, will deal with later
+    #roleName = guild.Guild.get_role(self, role_id = roleid)
 
-        if message.content.startswith('$myroles'):
-            print("$myroles request by:" + str(message.author))
-            role = CommonFuncs.getRoles(self, message)
-            print(role)
-            await message.channel.send("Your roles: {}".format(role))
+@bot.command()
+async def rmRole(ctx, *, roleid=None):
+    print(f"$rmRole request by {ctx.author}")
+    if roleid is None:
+        await ctx.send("```Help: This command removes permission for role members to post Coalition Missions\nUsage: $rmRole <roleID of role>```")
+        return
+    #checks perms with su users i.e discord admins
+    if funcs.checkPerms(ctx, suRoles):
+        allowed_roles.remove(str(roleid))
+        await ctx.send(f'RoleID {roleid} has been removed')
+    else:
+        await ctx.send("You do not have neccesary permissions")
+    #Having some issues with this code line, will deal with later
+    #roleName = guild.Guild.get_role(self, role_id = roleid)
+
+@bot.command()
+async def myroles(ctx, *, member : discord.Member=None):
+    print("$myroles request by:" + str(ctx.author))
+    if member is None:
+        member = ctx.author
+    role = funcs.getRoles(member)
+    await ctx.send("Your roles: " + str(role))
+
+@bot.command()
+async def missionAdd(ctx, *args):
+    help = """```This command creates Coalition missions\nUsage: !missionCreate [title];
+[description: Supports standard discord formatting.];\n
+[category: "BGS"/"Research"];
+[status: "recruiting"/"progress"/"success"/"failed"];
+[agents: @ yourself or agent(s) in charge];\n
+[personnel];
+[skills beyond category];
+[timeframe];
+[platform];```"""
+
+    if funcs.checkPerms(ctx, allowed_roles) is False:
+        await ctx.send("You need appropriate permissions")
+        return
+    if (len(args) == 0):
+        await ctx.send(help)
+        return
 
 
-client = MissionBot()
 config = dotenv_values('.env')
 client.run(config['TOKEN'])
