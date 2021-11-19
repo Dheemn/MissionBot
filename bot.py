9fd8c83f5c1192ec522b247f4b726b12cabd4860
Dheemn <42458390+Dheemn@users.noreply.github.com>
2021-11-19 21:21:26 +0530

    Update to bot.py
    Added JSON functionality to store settings, and changed logging type

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
-import datetime
 import discord
+import json
 import logging
 import sys
 
@@ -9,8 +9,8 @@ from discord.ext import commands
 from dotenv import dotenv_values
 
 class CommonFuncs():
-    def __init__(self):
-        pass
+    def __init__(self, l):
+        self._l = l
 
     #Get roles for user
     def getRoles(self, m, t : str):
@@ -18,7 +18,7 @@ class CommonFuncs():
         if t=='id':
             for x in (m.roles):
                 role.append(str(x.id))
-            role.remove('<@everyone roleID>')
+            role.remove('828538918823657503')
         else:
             for x in (m.roles):
                 role.append(str(x.name))
@@ -63,81 +63,102 @@ class CommonFuncs():
         msg = await channel.send(embed=embed)
         return msg.id
 
+    def get_jdata(self, file='settings.json', data=''):
+        if data == '':
+            raise Exception('data cannot be null')
+        try: 
+            data = data.split('.')
+            f = open(file, 'r')
+            js = json.load(f)
+            f.close()
+            for i in data:
+                js = js[i]
+            return js
+        except Exception as e:
+            self._l.error(f'Error: {e}')
+            raise e
 
 def main():
 
     intents = discord.Intents(messages = True, members = True, guilds = True)
     bot = commands.Bot(command_prefix='$', intents=intents)
 
-    logging.basicConfig(filename='bot.log', format='%(levelname)s:%(message)s', level=logging.INFO)
+    #logging.basicConfig(filename='bot.log', format='%(levelname)s:%(message)s', level=logging.INFO)
+    loggy = logging.getLogger(__name__)
+    loggy.setLevel(logging.INFO)
+    formatter = logging.Formatter('[%(asctime)s]: %(levelname)s:%(name)s %(message)s')
+    lh = logging.FileHandler('bot.log')
+    lh.setFormatter(formatter)
+    lh.setLevel(logging.INFO)
+    loggy.addHandler(lh)
 
-    funcs = CommonFuncs()
+    funcs = CommonFuncs(l=loggy)
 
-    ANNOUNCMENT_CHANNEL = <announcement_channel>
-    ADMIN_CHANNEL = <admin_channel>
+    ANNOUNCMENT_CHANNEL = funcs.get_jdata(data='settings.channels.ANNOUNCEMENT_CHANNEL')
+    ADMIN_CHANNEL = funcs.get_jdata(data='settings.channels.ADMIN_CHANNEL')
 
-    suRoles = []
-    allowed_roles = []
+    suRoles = funcs.get_jdata(data='settings.roles.suRoles')
+    allowed_roles = funcs.get_jdata(data='settings.roles.allowed_roles')
 
     @bot.event
     async def on_ready():
-        logging.info(f'[{datetime.datetime.now()}]: Logged in as {bot.user.name}{bot.user.id}')
+        loggy.info(f'Logged in as {bot.user.name}{bot.user.id}')
         #print(f'[{datetime.datetime.now()}]: Logged in as {bot.user.name}{bot.user.id}')
 
     @bot.command()
     async def ping(ctx):
         #print(f'[{datetime.datetime.now()}]: {ctx.author} pinged me!!!')
-        logging.info(f'[{datetime.datetime.now()}]: {ctx.author} pinged me!!!')
+        loggy.info(f'{ctx.author} pinged me!!!')
         await ctx.send(f'Pong! {round(bot.latency * 1000)}ms')
 
     @bot.command()
     #@commands.has_any_role(suRoles)
     async def addRole(ctx, *, roleid=None):
-        logging.info(f"[{datetime.datetime.now()}]: $addRole request by {ctx.author}")
+        loggy.info(f"$addRole request by {ctx.author}")
         #checks perms with su users i.e discord admins
         if funcs.checkPerms(ctx, suRoles) is False:
             await ctx.reply("You do not have neccesary permissions")
-            logging.info(f'[{datetime.datetime.now()}]: {ctx.author} tried using $addRole command. Denied!')
+            loggy.info(f'{ctx.author} tried using $addRole command. Denied!')
             return
         if roleid is None:
             await ctx.reply("```Help: This command gives permission to role members to post Coalition Missions\nUsage: $addRole <roleID of role>```")
             return
         allowed_roles.append(str(roleid))
-        logging.info(f'[{datetime.datetime.now()}]: RoleID {roleid} has been added to inclusion zone by {ctx.author}')
+        loggy.info(f'RoleID {roleid} has been added to inclusion zone by {ctx.author}')
         await ctx.reply(f'RoleID {roleid} has been added')
         
     @bot.command()
     #@commands.has_any_role(su)
     async def rmRole(ctx, *, roleid=None):
-        logging.info(f"[{datetime.datetime.now()}]: $rmRole request by {ctx.author}")
+        loggy.info(f"$rmRole request by {ctx.author}")
         #checks perms with su users i.e discord admins
         if funcs.checkPerms(ctx, suRoles) is False:
             await ctx.reply("You do not have neccesary permissions to perform action!")
-            logging.info(f'[{datetime.datetime.now()}]: {ctx.author} tried using $rmRole command. Denied!')
+            loggy.info(f'{ctx.author} tried using $rmRole command. Denied!')
             return
         if roleid is None:
             await ctx.reply("```Help: This command gives permission to role members to post Coalition Missions\nUsage: $addRole <roleID of role>```")
             return
         allowed_roles.remove(str(roleid))
-        logging.info(f'[{datetime.datetime.now()}]: RoleID {roleid} has been removed from inclusion zone by {ctx.author}')
+        loggy.info(f'RoleID {roleid} has been removed from inclusion zone by {ctx.author}')
         await ctx.reply(f'RoleID {roleid} has been removed')
 
     @bot.command()
     async def myroles(ctx, *, member : discord.Member=None):
-        logging.info(f'[{datetime.datetime.now()}]: $myroles request by: {ctx.author}')
+        loggy.info(f'$myroles request by: {ctx.author}')
         if member is None:
             member = ctx.author
         role = funcs.getRoles(member, 'name')
-        logging.info(msg=f'[{datetime.datetime.now()}]: $myroles request by: {ctx.author}')
         await ctx.send("Your roles: " + str(role))
 
     @bot.command()
-    async def clear(ctx, amount: int = 2):
-        await ctx.channel.purge(limit=amount)
+    async def clear(ctx, amount: int = 1):
+        loggy.info(f'{ctx.author} used clear command, {amount} msgs deleted')
+        await ctx.channel.purge(limit=(amount+1))
 
     @bot.command()
     async def missionCreate(ctx, *args):
-        help = """```This command creates Coalition missions\nUsage: $missionCreate [title];
+        help_p = """```This command creates Coalition missions\nUsage: $missionCreate \"[title];
 [description: Supports standard discord formatting.];\n
 [category: "BGS"/"Research"];
 [status: "recruiting"/"progress"/"success"/"failed"];
@@ -145,15 +166,15 @@ def main():
 [personnel];
 [skills beyond category];
 [timeframe];
-[platform];```"""
+[platform]\"\n\nPlease enclose the mission parameters in single(\') or double(\") quotes```"""
 
-        logging.info(f"[{datetime.datetime.now()}]: $missionCreate request by: {ctx.author}")
+        loggy.info(f"$missionCreate request by: {ctx.author}")
         if funcs.checkPerms(ctx, allowed_roles) is False:
-            logging.info(f"[{datetime.datetime.now()}]: $missionCreate request by: {ctx.author} was denied")
+            loggy.info(f"$missionCreate request by: {ctx.author} was denied")
             await ctx.send("You need appropriate permissions")
             return
         if (len(args) == 0):
-            await ctx.send(help)
+            await ctx.send(help_p)
             return
         message = str(args[0])
         mission = message.split(';')
@@ -164,13 +185,13 @@ def main():
         author = (ctx.author.display_name, ctx.author.avatar_url, ctx.author.id)
         await ctx.send(f':white_check_mark: Mission created with messageID {msg_ID}')
         await funcs.createEmbed(title='Mission Created',author=author, reason=reason, channel=bot.get_channel(ADMIN_CHANNEL))
-        logging.info(f"[{datetime.datetime.now()}]: $missionCreate request by: {ctx.author} granted and success with msgID: {msg_ID}")
+        loggy.info(f"$missionCreate request by: {ctx.author} granted and success with msgID: {msg_ID}")
 
     @bot.command()
     async def missionEdit(ctx, message: commands.MessageConverter, arg):
-        logging.info(f"[{datetime.datetime.now()}]: $missionEdit request by: {ctx.author}")
+        loggy.info(f"$missionEdit request by: {ctx.author}")
         if funcs.checkPerms(ctx, allowed_roles) is False:
-            logging.info(f"[{datetime.datetime.now()}]: $missionCreate request by: {ctx.author} was denied")
+            loggy.info(f"$missionCreate request by: {ctx.author} was denied")
             await ctx.send("You need appropriate permissions")
             return
         mission = arg.split(';')
@@ -182,11 +203,11 @@ def main():
         author = (ctx.author.display_name, ctx.author.avatar_url, ctx.author.id)
 
         await funcs.createEmbed(author=author, reason=reason, channel=bot.get_channel(ADMIN_CHANNEL))
-        logging.info(f"[{datetime.datetime.now()}]: $missionCreate request by: {ctx.author} granted and success with msgID: {message.id}")
+        loggy.info(f"$missionCreate request by: {ctx.author} granted and success with msgID: {message.id}")
 
     @missionEdit.error
     async def missionEdit_error(ctx, error):
-        help = """```This command edits Coalition missions\nUsage: $missionEdit [MessageID of Mission] "[title];
+        help_E = """```This command edits Coalition missions\nUsage: $missionEdit [MessageID of Mission] "[title];
 [description: Supports standard discord formatting.];\n
 [category: "BGS"/"Research"];
 [status: "recruiting"/"progress"/"success"/"failed"];
@@ -195,25 +216,24 @@ def main():
 [skills beyond category];
 [timeframe];
 [platform];"```"""
-        print(f"[{datetime.datetime.now()}]: $missionEdit error by: {ctx.author}")
         if isinstance(error, commands.MissingRequiredArgument):
-            logging.error(f'[{datetime.datetime.now()}]: $missionEdit Error: {error}')
-            await ctx.send(f'Missing Arguments \n{help}')
+            loggy.error(f'$missionEdit Error: {error}')
+            await ctx.send(f'Missing Arguments \n{help_E}')
             return
         if isinstance(error, commands.MessageNotFound):
-            logging.error(f'[{datetime.datetime.now()}]: $missionEdit Error: {error}')
+            loggy.error(f'$missionEdit Error: {error}')
             await ctx.send('Oops! Something went wrong. Please provide valid message ID with channelID. Eg: <channelID-messageID> or its URL')
             return
 
     @bot.command()
     async def missionRM(ctx, message: commands.MessageConverter):
-        logging.info(f"[{datetime.datetime.now()}]: $missionRM request by: {ctx.author}")
+        loggy.info(f"$missionRM request by: {ctx.author}")
         if funcs.checkPerms(ctx, allowed_roles) is False:
-            logging.info(f"[{datetime.datetime.now()}]: $missionRM request by: {ctx.author} was denied")
+            loggy.info(f"$missionRM request by: {ctx.author} was denied")
             await ctx.send("You need appropriate permissions")
             return
         if message.channel.id != ANNOUNCMENT_CHANNEL:
-            logging.info(f'[{datetime.datetime.now()}]: User {ctx.author} tried deleting message outside of announcement channel')
+            loggy.info(f'User {ctx.author} tried deleting message outside of announcement channel')
             await ctx.send('You cannot delete messages outside the Coalition Mission channel')
             return
 
@@ -225,16 +245,16 @@ def main():
 
         await ctx.send(f':white_check_mark: Mission with messageID:{msg_id} successfully deleted')
         await funcs.createEmbed(title='Message Deleted', author=author, reason=reason, channel=bot.get_channel(ADMIN_CHANNEL))
-        logging.info(f'[{datetime.datetime.now()}]: User {ctx.author} deleted mission with msgID {msg_id} successfully')
+        loggy.info(f'User {ctx.author} deleted mission with msgID {msg_id} successfully')
 
     @missionRM.error
     async def missionRM_error(ctx, error):
         if isinstance(error, commands.MissingRequiredArgument):
-            logging.error(f'[{datetime.datetime.now()}]: $missionRM Error: {error}')
+            loggy.error(f'$missionRM Error: {error}')
             await ctx.send('```Missing argument\nThis command deletes the coalition mission\n Usage: $missionRM [messageID/URL of the mission]```')
             return
         if isinstance(error, commands.MessageNotFound):
-            logging.error(f'[{datetime.datetime.now()}]: $missionRM Error: {error}')
+            loggy.error(f'$missionRM Error: {error}')
             await ctx.send('Oops! Something went wrong. Please provide valid message ID with channelID. Eg: <channelID-messageID> or its URL')
             return
     
@@ -250,24 +270,25 @@ def main():
         #error = getattr(error, 'original', error)
         
         if isinstance(error, commands.CommandNotFound):
-            logging.error(f'[{datetime.datetime.now()}]: {ctx.author} tried executing {ctx.message.content}, ERROR: not found')
+            loggy.error(f'{ctx.author} tried executing {ctx.message.content}, ERROR: not found')
             await ctx.send(f'Command {ctx.message.content} not found')
             return
         if isinstance(error, commands.DisabledCommand):
-            logging.error(f'[{datetime.datetime.now()}]: {ctx.author} tried executing disabled command: {ctx.message.content}')
+            loggy.error(f'{ctx.author} tried executing disabled command: {ctx.message.content}')
             await ctx.send(f'{ctx.message.content} has been disabled.')
             return
         if isinstance(error, discord.DiscordException):
-            logging.error(f'[{datetime.datetime.now()}]: Error: {error}')
+            loggy.error(f'Error: {error}')
             await ctx.send('Error has occured!! Please contact bot maintainer')
             return
-
-    config = dotenv_values('.env')
-    bot.run(config['TOKEN'])
+    
+    return bot
 
 if __name__=='__main__':
     try:
-        main()
-    except KeyboardInterrupt or SystemExit:
+        client = main()
+        config = dotenv_values('.env')
+        client.run(config['TOKEN'])
+    except KeyboardInterrupt:
         print('Exiting.....')
         sys.exit(0)
